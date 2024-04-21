ab31743ed745babfeb36cff028ae3888b4597fa0
MikeJeba <jebajovitha@gmail.com>
2024-04-21 19:14:37 +0530

    Firebase connected successfully

--- app/src/main/java/com/example/minigame/Result.java
+++ app/src/main/java/com/example/minigame/Result.java
@@ -1,5 +1,6 @@
 package com.example.minigame;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class Result extends AppCompatActivity {
 
@@ -28,7 +37,7 @@ public class Result extends AppCompatActivity {
         score = intent.getIntExtra("score",0);
         String userScore = String.valueOf(score);
         result.setText("Your score: "+userScore);
-
+        addQuoteToDB(userScore);
         playAgain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -44,6 +53,28 @@ public class Result extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 finish();
+            }
+        });
+
+    }
+
+    private void addQuoteToDB(String userScore) {
+        //create a hashmap
+        HashMap<String, Object> quoteHashmap = new HashMap<>();
+        quoteHashmap.put("userScore",userScore);
+
+        //instantiate database connection
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference quotesRef = database.getReference("userScore");
+
+        String key = quotesRef.push().getKey();
+        quoteHashmap.put("key",key);
+
+        quotesRef.child(key).setValue(quoteHashmap).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(Result.this, "Added", Toast.LENGTH_SHORT).show();
+
             }
         });
 
